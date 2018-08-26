990e783504536afadd0f5282ad8591081a5f7d53
Jacob Leon Lyerla <umasour@yahoo.com>
2018-08-26 13:51:46 -0700

    dmg loweed

--- src/dungeons/battle.js
+++ src/dungeons/battle.js
@@ -251,7 +251,7 @@ class Battle extends Component {
                 this.state.monster.level -
                 this.state.player.level 
             ) {
-              dmg.health = this.state.tempPlayer.health - Math.round(damage[0].damage * this.state.player.level *50 ) ;
+              dmg.health = this.state.tempPlayer.health - Math.round(damage[0].damage * this.state.player.level *2 ) ;
               dmg.combat = "hit";
             } else {
               dmg.combat = "missed";
@@ -556,7 +556,7 @@ class Battle extends Component {
             <br />
             Cost : {attack.cost}
             <br />
-            Damage: {Math.round(attack.damage * this.state.player.level *50) }
+            Damage: {Math.round(attack.damage * this.state.player.level *2) }
             <br />
             <br />
             <div>{attack.description}</div>
