fe86d0882152f256b44776229d45038510ff9c39
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-04 23:56:56 -0700

    main page is already looking a lot better

--- src/player/player.js
+++ src/player/player.js
@@ -1,5 +1,14 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
+import {
+  Form,
+  Alert,
+  Label,
+  Input,
+
+  Col,
+  Row
+} from "reactstrap";
 import { Link } from "react-router-dom";
 import Map from "../dungeons/map.js";
 class Player extends Component {
@@ -7,52 +16,55 @@ class Player extends Component {
     return (
       <Fragment>
         <div className="loginbackground-styles">
-          <br />
-          <br />
-          <br />
-              <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
+       
+     
+ 
           <div className="title-styles">Dungeon Race</div>
    
    
-          <br />
-          <br />
-          <br />
 
-          <br />
-          <br />
+          <Form className="login-styles">
 
-          <form className="login-styles">
-            Name:
-            <input
-              className="loginInput-styles"
-              name="name"
-              placeholder="Enter a name"
-              value={this.props.name}
-              onChange={this.props.handleInput}
-            />
-            Password:
-            <input
-              className="loginInput-styles"
-              name="password"
-              type="password"
-              placeholder="Enter a password"
-              value={this.props.password}
-              onChange={this.props.handleInput}
-            />
-            <br />
+          <Row style={{ margin: "1%" }}>
+              <Col md="6">
+                <Label for="name" style={{ width: "90%" }} className="text-left">
+                  Name:
+                </Label>
+
+                <Input
+                     className="loginInput-styles"
+                     name="name"
+                     id="name"
+                     placeholder="Enter a name"
+                     value={this.props.name}
+                     onChange={this.props.handleInput}
+                />
+              </Col>
+              <Col md="6">
+                <Label
+                  style={{ width: "90%" }}
+                  className="text-left"
+                  for="email"
+                >
+                  Email:
+                </Label>
+
+                <Input
+                className="loginInput-styles"
+                name="password"
+                type="password"
+                id="password"
+                placeholder="Enter a password"
+                value={this.props.password}
+                onChange={this.props.handleInput}
+                />
+              </Col>
+            </Row>
+        
             <Link to="/">
               <button
                 className="btn"
-                onClick={() =>
-                  this.props.loadPlayer(this.props.name, this.props.password)
-                }
+               type="submit"
               >
                 Log On
               </button>
@@ -60,7 +72,7 @@ class Player extends Component {
             <Link to="/create">
               <button className="btn">Create</button>
             </Link>
-          </form>
+          </Form>
         </div>
       </Fragment>
     );
