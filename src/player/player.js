c77f32034f62a2b49defd00b131a1ef542a12a32
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-05 18:04:51 -0700

    just running a format

--- src/player/player.js
+++ src/player/player.js
@@ -1,46 +1,39 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
-import {
-  Form,
-  Alert,
-  Label,
-  Input,
-
-  Col,
-  Row
-} from "reactstrap";
+import { Form, Alert, Label, Input, Col, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 import Map from "../dungeons/map.js";
 class Player extends Component {
   render() {
     return (
       <Fragment>
-       
         <div className="loginbackground-styles">
-        <br/>
-     
- 
+          <br />
+
           <div className="title-styles">Dungeon Race</div>
-   
-   
 
-          <Form className="login-styles"  onClick={(e) =>
-                  this.props.loadPlayer(e,this.props.name, this.props.password)
-                }>
-
-          <Row style={{ margin: "1%" }}>
+          <Form
+            className="login-styles"
+            onClick={e =>
+              this.props.loadPlayer(e, this.props.name, this.props.password)
+            }
+          >
+            <Row style={{ margin: "1%" }}>
               <Col md="6">
-                <Label for="name" style={{ width: "90%" }} className="text-left">
+                <Label
+                  for="name"
+                  style={{ width: "90%" }}
+                  className="text-left"
+                >
                   Name:
                 </Label>
 
                 <Input
-                     className="loginInput-styles"
-                     name="name"
-                     id="name"
-                     placeholder="Enter a name"
-                     value={this.props.name}
-                     onChange={this.props.handleInput}
+                  className="loginInput-styles"
+                  name="name"
+                  id="name"
+                  value={this.props.name}
+                  onChange={this.props.handleInput}
                 />
               </Col>
               <Col md="6">
@@ -49,29 +42,25 @@ class Player extends Component {
                   className="text-left"
                   for="email"
                 >
-                  Email:
+                  Password:
                 </Label>
 
                 <Input
-                className="loginInput-styles"
-                name="password"
-                type="password"
-                id="password"
-                placeholder="Enter a password"
-                value={this.props.password}
-                onChange={this.props.handleInput}
+                  className="loginInput-styles"
+                  name="password"
+                  type="password"
+                  id="password"
+                  value={this.props.password}
+                  onChange={this.props.handleInput}
                 />
               </Col>
             </Row>
-        
+
             <Link to="/">
-              <button
-                className="btn"
-               type="submit"
-              >
+              <button className="btn" type="submit">
                 Log On
               </button>
-              <br/>
+              <br />
             </Link>
             <Link to="/create">
               <button className="btn btn-register">Create An Adventurer</button>
