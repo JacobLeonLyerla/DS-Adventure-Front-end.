094a0063bbf3d48c8c2a09212d4187430a21f358
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-07 23:18:48 -0700

    Set up branch in gitkraken making sure it is working correctly

--- src/dungeons/battle.js
+++ src/dungeons/battle.js
@@ -1,12 +1,20 @@
 import React, { Component, Fragment } from "react";
+
 import axios from "axios";
+
 import { Row, Col } from "reactstrap";
+
 import { Redirect } from "react-router-dom";
+
 // import paladin from "./img/paladinportrait.png";
 import rogue from "../assets/rogueportrait.jpg";
+
 import mage from "../assets/mageportrait.png";
+
 import warrior from "../assets/warriorportrait.png";
+
 import ranger from "../assets/rangerportrait.png";
+
 import necro from "../assets/necromancerportrait.jpg";
 
 import { Progress } from "reactstrap";
