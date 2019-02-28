087c3b55d5f65099a6cf5a2e01aa05886df82c81
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-27 23:55:23 -0700

    added comments to function in common helpers

--- src/helpers/commonHelpers.js
+++ src/helpers/commonHelpers.js
@@ -1,3 +1,5 @@
+  // this takes the event and sets it on state, the event here is the
+  // onChange from my input fields
 export function handleInput(input) {
     this.setState({ [input.target.name]: input.target.value });
   
