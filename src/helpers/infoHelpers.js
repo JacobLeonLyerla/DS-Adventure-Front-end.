7feaa66a79c9eb4877ae19b4d73adaf849776036
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-01 23:15:01 -0700

    update player helper added

--- src/helpers/infoHelpers.js
+++ src/helpers/infoHelpers.js
@@ -1,10 +1,31 @@
 import axios from "axios";
-  // take id from the  component did mount use it do grab the player from the database than
-  // set that data on state
-  export function setCurrentPlayer(id){
-    axios
-      .get(`https://dungeon-run.herokuapp.com/players/${id}`)
-      .then(response => {
-        this.setState({ player: response.data, preview: response.data.idle });
-      })
-  };
+// take id from the  component did mount use it do grab the player from the database than
+// set that data on state
+export function setCurrentPlayer(id) {
+  axios
+    .get(`https://dungeon-run.herokuapp.com/players/${id}`)
+    .then(response => {
+      this.setState({ player: response.data, preview: response.data.idle });
+    });
+}
+
+// this was going to allow people to edit player info, however it is not currently
+// in the game because emplementing it was a bit wonky i'll try something a bit later
+export function updatePlayer(id) {
+  const player = {};
+  if (this.state.name !== "") {
+    player.name = this.state.name;
+  }
+  if (this.state.bio !== "") {
+    player.bio = this.state.bio;
+  }
+  axios
+    .put(`https://dungeon-run.herokuapp.com/players/${id}`, player)
+    .then(() => {
+      this.setState({
+        name: "",
+        bio: ""
+      });
+      this.setCurrentPlayer(id);
+    });
+}
