a03f703b59f7a93204f0621a763a47ec86b7a186
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-05 23:31:50 -0700

    helper added for current room

--- src/helpers/loadDHelper.js
+++ src/helpers/loadDHelper.js
@@ -1,16 +1,66 @@
 import axios from "axios";
 
-export function setDungeon (reload) {
-    if (this.state.player.currentLocation === undefined) {
-      axios
-        .get("https://dungeon-run.herokuapp.com/blackheart")
-        .then(response => {
-          this.currentRoom(response.data[0]._id);
-          this.setState({ players: response.data });
 
-          if (reload === "reload") {
-            // window.location.reload();
+
+export function currentRoom(id) {
+  axios
+    .get(`https://dungeon-run.herokuapp.com/blackheart/${id}`)
+    .then(response => {
+      this.setState({ area: response.data });
+      if (this.state.moved === true) {
+        this.setState({ moved: false });
+        let battle = {};
+        if (this.state.area.monsters) {
+          let randomInt = this.getRandomInt(700);
+          if (randomInt <= 250 && this.state.area.monsters.length >= 1) {
+            battle.currentBattle = this.state.area.monsters[0];
+          } else if (
+            randomInt >= 251 &&
+            randomInt <= 450 &&
+            this.state.area.monsters.length >= 2
+          ) {
+            battle.currentBattle = this.state.area.monsters[1];
+          } else if (
+            randomInt >= 451 &&
+            randomInt <= 600 &&
+            this.state.area.monsters.length >= 3
+          ) {
+            battle.currentBattle = this.state.area.monsters[2];
+          } else if (
+            randomInt >= 600 &&
+            randomInt <= 700 &&
+            this.state.area.monsters.length >= 4
+          ) {
+            battle.currentBattle = this.state.area.monsters[3];
+          } else if (
+            randomInt >= 0 &&
+            randomInt <= 1000 &&
+            this.state.area.monsters.length >= 5
+          ) {
+            battle.currentBattle = this.state.area.monsters[4];
+          } else {
+            battle.currentBattle = [];
+            this.setState({
+              redirect: false
+            });
           }
-        })
-    }
+
+          axios
+            .put(
+              `https://dungeon-run.herokuapp.com/players/${
+                this.state.player._id
+              }`,
+              battle
+            )
+
+            .then(response => {
+              if (response.data.currentBattle.length > 0) {
+                this.setState({
+                  redirect: true
+                });
+              }
+            });
+        }
+      }
+    });
 }
