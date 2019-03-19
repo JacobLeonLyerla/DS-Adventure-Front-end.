6c3e05d20c768c4dbb041d811f301e7bb214abb1
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-18 23:16:24 -0700

    created set dungeon helper

--- src/helpers/loadDHelper.js
+++ src/helpers/loadDHelper.js
@@ -187,3 +187,25 @@ winnings() {
     );
   }
 }
+
+export function setDungeon ( reload ) {
+  if (this.state.player.currentLocation === undefined) {
+    axios
+      .get("https://dungeon-run.herokuapp.com/blackheart")
+      .then(response => {
+        this.currentRoom(response.data[0]._id);
+        this.setState({ players: response.data });
+
+        if (reload === "reload") {
+          // window.location.reload();
+        }
+      })
+
+      .catch(err => {});
+  } else {
+    this.currentRoom(this.state.player.currentLocation._id);
+    if (reload === "reload") {
+      // window.location.reload();
+    }
+  }
+};
