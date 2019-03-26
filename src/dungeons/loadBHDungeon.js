23d49cd5e2a5ad493cec31fe508346d27360d7ad
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-25 23:43:30 -0700

    removed path and toggle and used helpers for them

--- src/dungeons/loadBHDungeon.js
+++ src/dungeons/loadBHDungeon.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 
 import { Redirect } from "react-router-dom";
 
-import { Modal, Col, Row } from "reactstrap";
+import { toggle } from "../helpers/commonHelpers";
 import {
   currentRoom,
   setLocation,
@@ -12,12 +12,9 @@ import {
   getRandomInt,
   movmentRender,
   ding,
-  currentPlayer
+  currentPlayer,
+  path
 } from "../helpers/loadDHelper";
-import axios from "axios";
-
-import Map from "../dungeons/map.js";
-
 class BlackHeart extends Component {
   constructor() {
     super();
@@ -42,14 +39,6 @@ class BlackHeart extends Component {
 
       modal: false
     };
-
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
   }
 
   componentDidMount() {
@@ -57,69 +46,7 @@ class BlackHeart extends Component {
     this.currentPlayer(id);
   }
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //************************************************** AXIOS
 
-
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //************************************************** MOVMENT
-  path(direction) {
-    let pathId = "";
-
-    switch (direction) {
-      case "North":
-        if (this.state.area.north.length > 0) {
-          for (let index = 0; index < this.state.area.north.length; index++) {
-            pathId = this.state.area.north[0]._id;
-          }
-        } else {
-          pathId = "No path";
-        }
-
-        break;
-      case "South":
-        if (this.state.area.south.length > 0) {
-          for (let index = 0; index < this.state.area.south.length; index++) {
-            pathId = this.state.area.south[0]._id;
-          }
-        } else {
-          pathId = "No path";
-        }
-
-        break;
-      case "East":
-        if (this.state.area.east.length > 0) {
-          for (let index = 0; index < this.state.area.east.length; index++) {
-            pathId = this.state.area.east[0]._id;
-          }
-        } else {
-          pathId = "No path";
-        }
-
-        break;
-      case "West":
-        if (this.state.area.west.length > 0) {
-          for (let index = 0; index < this.state.area.west.length; index++) {
-            pathId = this.state.area.west[0]._id;
-          }
-        } else {
-          pathId = "No path";
-        }
-
-        break;
-      default:
-        break;
-    }
-
-    return pathId;
-  }
-
-  renderRedirect = id => {
-    if (this.state.redirect) {
-      return <Redirect to={`/battle/${id}`} />;
-    }
-  };
 
   render() {
     this.currentRoom = currentRoom.bind(this);
@@ -130,7 +57,9 @@ class BlackHeart extends Component {
     this.getRandomInt = getRandomInt;
     this.movmentRender = movmentRender.bind(this);
     this.ding = ding;
-    this.currentPlayer = currentPlayer.bind(this)
+    this.currentPlayer = currentPlayer.bind(this);
+    this.path = path;
+    this.toggle = toggle.bind(this);
     return (
       <Fragment>
         {this.renderRedirect(this.state.player._id)}
