91ff6116f853384da9d6f097f34de8a5d90a3057
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-26 21:23:56 -0700

    created current room helper

---
+++ src/helpers/itemsHelper.js
@@ -0,0 +1,33 @@
+
+ import axios from "axios";
+ export function currentRoom ( id ) {
+    axios
+      .get(`https://dungeon-run.herokuapp.com/blackheart/${id}`)
+      .then(response => {
+        this.setState({ area: response.data });
+        let roomLoot = {};
+        let roomFilter = this.state.area.items.filter(item => {
+          let failed = false;
+          let secondfailed = false;
+          this.state.player.items.forEach(playerItem => {
+            if (playerItem._id !== item._id) {
+              return;
+            } else {
+              return (failed = true);
+            }
+          });
+          this.state.player.gear.forEach(InventoryItem => {
+            if (InventoryItem._id !== item._id) {
+              return;
+            } else {
+              return (secondfailed = true);
+            }
+          });
+
+          return failed === false && secondfailed === false;
+        });
+
+        roomLoot.items = roomFilter;
+        this.setState({ area: { items: roomLoot.items } });
+      });
+  };
