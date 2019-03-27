4652c6cbb49568c0b0eb481047cfd2e24ebf9e88
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-26 21:27:49 -0700

    removed delete item function added it to helpers

--- src/helpers/itemsHelper.js
+++ src/helpers/itemsHelper.js
@@ -39,3 +39,104 @@ export function currentPlayer ( id ) {
         this.currentRoom(this.state.player.currentLocation._id);
       });
   };
+
+  export default deleteItem(type, loot) {
+    console.log(loot, type);
+    let items = {};
+    switch (type) {
+      case "Loot":
+        items.items = [];
+
+        this.state.player.items.push(loot._id);
+
+        items.items = this.state.player.items;
+
+        axios
+          .put(
+            `https://dungeon-run.herokuapp.com/players/${
+              this.state.player._id
+            }`,
+            items
+          )
+          .then(() => {
+            axios
+              .get(
+                `https://dungeon-run.herokuapp.com/players/${
+                  this.state.player._id
+                }`
+              )
+              .then(response => {
+                this.setState({ player: response.data });
+                this.currentRoom(this.state.player.currentLocation._id);
+              });
+          });
+
+        break;
+      case "Inventory":
+      case "Equip":
+        if (type === "Inventory") {
+          items.items = this.state.player.items.filter(
+            item => item._id !== loot._id
+          );
+
+          this.state.player.gear.push(loot._id);
+
+          items.gear = this.state.player.gear;
+
+          items.items = this.state.player.items;
+
+          items[loot.slot] = "Equipped";
+
+          items.health = this.state.player.health += loot.health;
+
+          items.endurance = this.state.player.endurance += loot.endurance;
+
+          items.intellect = this.state.player.intellect += loot.intellect;
+
+          items.strength = this.state.player.strength += loot.strength;
+
+          items.agility = this.state.player.agility += loot.agility;
+        } else if (type === "Equip") {
+          items.gear = this.state.player.gear.filter(
+            item => item._id !== loot._id
+          );
+          this.state.player.items.push(loot._id);
+
+          items.items = this.state.player.items;
+        }
+        break;
+      case "Drop Item":
+        items.items = this.state.player.items.filter(
+          item => item._id !== loot._id
+        );
+        break;
+
+      default:
+        break;
+    }
+
+    axios
+      .put(
+        `https://dungeon-run.herokuapp.com/players/${this.state.player._id}`,
+        items
+      )
+
+      .then(response => {
+        axios
+          .get(
+            `https://dungeon-run.herokuapp.com/players/${this.state.player._id}`
+          )
+
+          .then(response => {
+            this.setState({ player: response.data });
+            this.currentRoom(this.state.player.currentLocation._id);
+          })
+
+          .catch(err => {});
+        if (type === "Equip") {
+          this.removeEquipment(loot);
+        }
+
+        //window.location.reload();
+      });
+  }
