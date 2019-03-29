8e33b9a4f852f90c4c50c508475f018649460ec0
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-29 00:30:38 -0700

    moved my routes to a route helper, and added background function to helpers

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
 
-import { Route } from "react-router-dom";
-
 import "./App.css";
 
-import Player from "./player/player";
 
-import Create from "./player/createPlayer";
-
-import Info from "./player/info";
-
-import Equip from "./player/equipment";
-
-import DungeonList from "./dungeons/dungeonList";
-
-import BlackHeart from "./dungeons/loadBHDungeon";
-
-import Items from "./dungeons/items";
-
-import Battle from "./dungeons/battle";
+import {routes} from "./helpers/appRoutes";
 import {
   setPlayer,
   findPlayer,
   loadPlayer,
-  renderRedirect
+  renderRedirect,
+  bhbackground,
 } from "./helpers/appHelpers";
 import { handleInput } from "./helpers/commonHelpers";
 class App extends Component {
@@ -45,79 +31,20 @@ class App extends Component {
     this.setPlayer();
   }
 
-  // this background is only loaded if it's not on either of those sites so if it's on another one than it will load,
-  // it also loads on local host, because i have not cared enough to add local host in
-  bhbackground() {
-    if (
-      window.location.href !== "https://dungeon-run.netlify.com/create" &&
-      window.location.href !== "https://dungeon-run.netlify.com/"
-    ) {
-      return "blackHeartBackground-styles";
-    }
-  }
-
   // the render has all the routes contained in side of it.
   render() {
     this.setPlayer = setPlayer.bind(this);
     this.findPlayer = findPlayer.bind(this);
     this.loadPlayer = loadPlayer.bind(this);
     this.renderRedirect = renderRedirect.bind(this);
+    this.bhbackground = bhbackground
+    this.routes = routes.bind(this)
     this.handleInput = handleInput.bind(this);
+
     return (
       <div className="App">
         {this.renderRedirect(this.state.id)}
-        <Route
-          exact
-          path="/"
-          render={props => (
-            <Player
-              {...this.state}
-              handleInput={this.handleInput}
-              findPlayer={this.findPlayer}
-              loadPlayer={this.loadPlayer}
-            />
-          )}
-        />
-
-        <Route
-          path="/create"
-          render={props => <Create setPlayer={this.setPlayer} />}
-        />
-
-        <Route
-          path="/info/:id"
-          render={props => <Info {...props} player={this.state.player} />}
-        />
-
-        <Route
-          path="/dungeons/:id"
-          render={props => (
-            <DungeonList {...props} player={this.state.player} />
-          )}
-        />
-
-        <div className={this.bhbackground()}>
-          <Route
-            path="/blackheart/:id"
-            render={props => (
-              <BlackHeart {...props} player={this.state.player} />
-            )}
-          />
-
-          <Route
-            path="/blackheart/:id"
-            render={props => <Items {...props} player={this.state.player} />}
-          />
-
-          <Route
-            path="/battle/:id"
-            render={props => <Battle {...props} player={this.state.player} />}
-          />
-          <Route
-            path="/equip/:id"
-            render={props => <Equip {...props} player={this.state.player} />}
-          />
-        </div>
+      {this.routes()}
       </div>
     );
   }
