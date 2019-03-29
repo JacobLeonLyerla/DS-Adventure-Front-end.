ec11e13aaf5b258be99cf33bccde69d943bdfd14
Jacob Leon Lyerla <umasour@yahoo.com>
2019-03-29 00:01:55 -0700

    turned return into a function and moved it into helpers

--- src/dungeons/battle.js
+++ src/dungeons/battle.js
@@ -1,9 +1,6 @@
 import React, { Component, Fragment } from "react";
-import axios from "axios";
-import { Row, Col } from "reactstrap";
-import { Redirect } from "react-router-dom";
-
 import {
+  battleLayout,
   currentPlayer,
   setTemps,
   fetchTemps,
@@ -17,7 +14,8 @@ import {
   changeSpell,
   opponentAttacks,
   renderOpponentStats,
-  renderAdventurerStats
+  renderAdventurerStats,
+  renderRedirect
 } from "../helpers/battleHelpers";
 class Battle extends Component {
   state = {
@@ -32,7 +30,6 @@ class Battle extends Component {
     currentLocation: "",
     tempMonHP: 0,
     redirect: false,
-
     battle: false
   };
   componentDidMount() {
@@ -40,13 +37,8 @@ class Battle extends Component {
     this.currentPlayer(id);
   }
 
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to={`/blackheart/${this.state.player._id}`} />;
-    }
-  };
-
   render() {
+    this.battleLayout = battleLayout.bind(this);
     this.currentPlayer = currentPlayer.bind(this);
     this.setTemps = setTemps.bind(this);
     this.fetchTemps = fetchTemps.bind(this);
@@ -61,42 +53,9 @@ class Battle extends Component {
     this.opponentAttacks = opponentAttacks.bind(this);
     this.renderOpponentStats = renderOpponentStats.bind(this);
     this.renderAdventurerStats = renderAdventurerStats.bind(this);
+    this.renderRedirect = renderRedirect.bind(this);
 
-    return (
-      <Fragment>
-        {this.renderRedirect()}
-        <Row className="fighters">
-          <Col md="5">
-            {this.renderAdventurer()}
-            <br />
-            {this.renderAdventurerStats()}
-          </Col>
-          <Col md="5">
-            {this.renderOpponent()}
-            <br />
-            {this.renderOpponentStats()}
-          </Col>
-        </Row>
-
-        <Row className="middleBattle-styles">
-          <Col md="3" className="mid-col">
-            {this.adventurerAttacks()}
-          </Col>
-
-          <Col md="3" className="mid-col">
-            {this.renderStats()}
-          </Col>
-          <Col md="3" className="mid-col">
-            <div
-              className={this.state.monster.rarity + "Stats-styles mid-battle"}
-            >
-              {this.opponentAttacks()}
-            </div>
-          </Col>
-        </Row>
-        <br />
-      </Fragment>
-    );
+    return <Fragment>{this.battleLayout()}</Fragment>;
   }
 }
 
